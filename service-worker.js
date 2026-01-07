6d2a7b5a4f8045ff9e95ac497629c56d7f7f7380
nguyenduchieu84 <nguyenduchieu.hut@gmail.com>
2026-01-07 22:40:51 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'rt-iqi-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './Tra cuu IQI theo ISO v1.0.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
